155778904e80610821bf39685fc4945ee73b64e1
Richard Melo <rrvmelo@gmail.com>
2023-06-24 15:06:56 -0300

    correção do submenu do sidebar e inicio de modal

--- src/App.vue
+++ src/App.vue
@@ -9,9 +9,8 @@
         <v-icon icon="mdi-magnify"></v-icon>
       </v-btn>
 
-
       <v-divider></v-divider>
-      <v-list-subheader><strong>Menu</strong></v-list-subheader>
+      <v-list-subheader><span>Menu</span></v-list-subheader>
       <v-divider></v-divider>
       <v-list-item prepend-icon="mdi-home">Início</v-list-item>
       <v-list-item prepend-icon="mdi-inbox">Solicitaçõs em Análise</v-list-item>
@@ -25,124 +24,129 @@
       </v-list-item>
 
       <!--Ficha Informativa-->
-      <v-list-group value="Ficha Informativa">
-        <template #activator="{ props }">
-          <v-list-item v-bind="props" prepend-icon="mdi-file-outline">
-            Ficha Informativa
-          </v-list-item>
-        </template>
-        <v-list-item prepend-icon="mdi-format-list-bulleted">Listar Fichas</v-list-item>
-        <v-list-item prepend-icon="mdi-plus-thick">Relatório</v-list-item>
-        <v-list-item prepend-icon="mdi-information-outline">Sobre a Ficha Informativa</v-list-item>
-      </v-list-group>
+      <v-list>
+        <v-list-group value="Ficha Informativa">
+          <template #activator="{ props }">
+            <v-list-item v-bind="props" prepend-icon="mdi-file-outline" title="Ficha Informativa">
+            </v-list-item>
+          </template>
+          <v-list-item prepend-icon="mdi-format-list-bulleted">Listar Fichas</v-list-item>
+          <v-list-item prepend-icon="mdi-plus-thick">Relatório</v-list-item>
+          <v-list-item prepend-icon="mdi-information-outline">Sobre a Ficha Informativa</v-list-item>
+        </v-list-group>
+      
 
-      <!--FUMCRIA-->
-      <v-list-group value="FUMCRIA">
-        <template #activator="{ props }">
-          <v-list-item v-bind="props" prepend-icon="mdi-emoticon-happy-outline">
-            FUMCRIA
-          </v-list-item>
-        </template>
-        <v-list-item prepend-icon="mdi-format-list-bulleted">Listar Doações</v-list-item>
-        <v-list-item prepend-icon="mdi-plus-thick">Nova Doação</v-list-item>
-        <v-list-item prepend-icon="mdi-list-box-outline">Extrato de Doações</v-list-item>
-        <v-list-item prepend-icon="mdi-format-list-bulleted">Listar Doações ao FUMCRIA</v-list-item>
-        <v-list-item prepend-icon="mdi-information-outline">Sobre o FUMCRIA</v-list-item>
-        <v-list-item prepend-icon="mdi-gavel">Legislação do FUMCRIA</v-list-item>
-        <v-list-item prepend-icon="mdi-magnify">Lista de Entidades</v-list-item>
-        <v-list-item prepend-icon="mdi-help">Dúvidas Frequentes</v-list-item>
-      </v-list-group>
+        <!--FUMCRIA-->
+        <v-list-group value="FUMCRIA">
+          <template #activator="{ props }">
+            <v-list-item v-bind="props" prepend-icon="mdi-emoticon-happy-outline">
+              FUMCRIA
+            </v-list-item>
+          </template>
+          <v-list-item prepend-icon="mdi-format-list-bulleted">Listar Doações</v-list-item>
+          <v-list-item prepend-icon="mdi-plus-thick">Nova Doação</v-list-item>
+          <v-list-item prepend-icon="mdi-list-box-outline">Extrato de Doações</v-list-item>
+          <v-list-item prepend-icon="mdi-format-list-bulleted">Listar Doações ao FUMCRIA</v-list-item>
+          <v-list-item prepend-icon="mdi-information-outline">Sobre o FUMCRIA</v-list-item>
+          <v-list-item prepend-icon="mdi-gavel">Legislação do FUMCRIA</v-list-item>
+          <v-list-item prepend-icon="mdi-magnify">Lista de Entidades</v-list-item>
+          <v-list-item prepend-icon="mdi-help">Dúvidas Frequentes</v-list-item>
+        </v-list-group>
+      
 
-      <!--Agendar Serviço Presencial-->
-      <v-list-group value="Agendar Serviço Presencial">
-        <template #activator="{ props }">
-          <v-list-item v-bind="props" prepend-icon="mdi-calendar-month">
-            Agendar Serviço Presencial
-          </v-list-item>
-        </template>
-        <v-list-item prepend-icon="mdi-format-list-bulleted">Meus Agendamentos</v-list-item>
-        <v-list-item prepend-icon="mdi-plus-thick">Novo Agendamento</v-list-item>
-      </v-list-group>
+        <!--Agendar Serviço Presencial-->
+        <v-list-group value="Agendar Serviço Presencial">
+          <template #activator="{ props }">
+            <v-list-item v-bind="props" prepend-icon="mdi-calendar-month">
+              Agendar Serviço Presencial
+            </v-list-item>
+          </template>
+          <v-list-item prepend-icon="mdi-format-list-bulleted">Meus Agendamentos</v-list-item>
+          <v-list-item prepend-icon="mdi-plus-thick">Novo Agendamento</v-list-item>
+        </v-list-group>
 
-      <!--Licitações-->
-      <v-list-group value="Licitações">
-        <template #activator="{ props }">
-          <v-list-item v-bind="props" prepend-icon="mdi-file-document-outline">
-            Licitações
-          </v-list-item>
-        </template>
-        <v-list-item prepend-icon="mdi-format-list-bulleted">Listar Licitações</v-list-item>
-        <v-list-item prepend-icon="mdi-close-thick">Licitações Suspensas</v-list-item>
-        <v-list-item prepend-icon="mdi-check-bold">Licitações Encerradas</v-list-item>
-        <v-list-item prepend-icon="mdi-download">Instruções Fornecedores</v-list-item>
-      </v-list-group>
+        <!--Licitações-->
+        <v-list-group value="Licitações">
+          <template #activator="{ props }">
+            <v-list-item v-bind="props" prepend-icon="mdi-file-document-outline">
+              Licitações
+            </v-list-item>
+          </template>
+          <v-list-item prepend-icon="mdi-format-list-bulleted">Listar Licitações</v-list-item>
+          <v-list-item prepend-icon="mdi-close-thick">Licitações Suspensas</v-list-item>
+          <v-list-item prepend-icon="mdi-check-bold">Licitações Encerradas</v-list-item>
+          <v-list-item prepend-icon="mdi-download">Instruções Fornecedores</v-list-item>
+        </v-list-group>
 
-      <!--Fiscalizações-->
-      <v-list-group value="Fiscalizações">
-        <template #activator="{ props }">
-          <v-list-item v-bind="props" prepend-icon="mdi-progress-check">
-            Fiscalizações
-          </v-list-item>
-        </template>
-        <v-list-item prepend-icon="mdi-cash">Multas</v-list-item>
-      </v-list-group>
+        <!--Fiscalizações-->
+        <v-list-group value="Fiscalizações">
+          <template #activator="{ props }">
+            <v-list-item v-bind="props" prepend-icon="mdi-progress-check">
+              Fiscalizações
+            </v-list-item>
+          </template>
+          <v-list-item prepend-icon="mdi-cash">Multas</v-list-item>
+        </v-list-group>
 
-      <!--Zoneamento-->
-      <v-list-group value="Zoneamento">
-        <template #activator="{ props }">
-          <v-list-item v-bind="props" prepend-icon="mdi-map-outline">
-            Zoneamento
-          </v-list-item>
-        </template>
-        <v-list-item prepend-icon="mdi-format-list-bulleted">Consultar Viabilidade</v-list-item>
-      </v-list-group>
+        <!--Zoneamento-->
+        <v-list-group value="Zoneamento">
+          <template #activator="{ props }">
+            <v-list-item v-bind="props" prepend-icon="mdi-map-outline">
+              Zoneamento
+            </v-list-item>
+          </template>
+          <v-list-item prepend-icon="mdi-format-list-bulleted">Consultar Viabilidade</v-list-item>
+        </v-list-group>
 
-      <!--Meio Ambiente-->
-      <v-list-group value="Meio Ambiente">
-        <template #activator="{ props }">
-          <v-list-item v-bind="props" prepend-icon="mdi-leaf">
-            Meio Ambiente
-          </v-list-item>
-        </template>
-        <v-list-item prepend-icon="mdi-folder-open-outline">Solicitações Não Enviadas</v-list-item>
-        <v-list-item prepend-icon="mdi-plus-thick">Solicitar Serviço</v-list-item>
-        <v-list-item prepend-icon="mdi-domain">Empresas Credenciadas</v-list-item>
-      </v-list-group>
+        <!--Meio Ambiente-->
+        <v-list-group value="Meio Ambiente">
+          <template #activator="{ props }">
+            <v-list-item v-bind="props" prepend-icon="mdi-leaf">
+              Meio Ambiente
+            </v-list-item>
+          </template>
+          <v-list-item prepend-icon="mdi-folder-open-outline">Solicitações Não Enviadas</v-list-item>
+          <v-list-item prepend-icon="mdi-plus-thick">Solicitar Serviço</v-list-item>
+          <v-list-item prepend-icon="mdi-domain">Empresas Credenciadas</v-list-item>
+        </v-list-group>
 
-      <!--PROCON-->
-      <v-list-group value="PROCON">
-        <template #activator="{ props }">
-          <v-list-item v-bind="props" prepend-icon="mdi-alpha-p-circle-outline">
-            PROCON
-          </v-list-item>
-        </template>
-        <v-list-item prepend-icon="mdi-format-list-bulleted">Solicitações Retorno</v-list-item>
-        <v-list-item prepend-icon="mdi-plus-thick">Solicitar Serviço</v-list-item>
-        <v-list-item prepend-icon="mdi-information-outline">Sobre o PROCON</v-list-item>
-      </v-list-group>
+        <!--PROCON-->
+        <v-list-group value="PROCON">
+          <template #activator="{ props }">
+            <v-list-item v-bind="props" prepend-icon="mdi-alpha-p-circle-outline">
+              PROCON
+            </v-list-item>
+          </template>
+          <v-list-item prepend-icon="mdi-format-list-bulleted">Solicitações Retorno</v-list-item>
+          <v-list-item prepend-icon="mdi-plus-thick">Solicitar Serviço</v-list-item>
+          <v-list-item prepend-icon="mdi-information-outline">Sobre o PROCON</v-list-item>
+        </v-list-group>
 
-      <!--Pacicipação Cidadã-->
-      <v-list-group value="Pacicipação Cidadã">
-        <template #activator="{ props }">
-          <v-list-item v-bind="props" prepend-icon="mdi-chat-processing">
-            Pacicipação Cidadã
-          </v-list-item>
-        </template>
-        <v-list-item prepend-icon="mdi-chat-processing-outline">Participar</v-list-item>
-        <v-list-item prepend-icon="mdi-format-list-bulleted">Minhas manifestações - PPA</v-list-item>
-        <v-list-item prepend-icon="mdi-gavel">Votação LDO</v-list-item>
-        <v-list-item prepend-icon="mdi-format-list-bulleted">Resultado das Votações</v-list-item>
-      </v-list-group>
+        <!--Pacicipação Cidadã-->
+        <v-list-group value="Pacicipação Cidadã">
+          <template #activator="{ props }">
+            <v-list-item v-bind="props" prepend-icon="mdi-chat-processing">
+              Pacicipação Cidadã
+            </v-list-item>
+          </template>
+          <v-list-item prepend-icon="mdi-chat-processing-outline">Participar</v-list-item>
+          <v-list-item prepend-icon="mdi-format-list-bulleted">Minhas manifestações - PPA</v-list-item>
+          <v-list-item prepend-icon="mdi-gavel">Votação LDO</v-list-item>
+          <v-list-item prepend-icon="mdi-format-list-bulleted">Resultado das Votações</v-list-item>
+        </v-list-group>
+      </v-list>
 
-      <!--Sobre o Fácil-->
-      <v-list-item prepend-icon="mdi-information">Sobre Fácil</v-list-item>
+        <!--Sobre o Fácil-->
+        <v-list-item prepend-icon="mdi-information">Sobre Fácil</v-list-item>
     </v-navigation-drawer>
+
+
 
     <!--HEADER-->
     <v-app-bar flat class="border-b bg-light-blue-darken-1">
       <v-app-bar-nav-icon @click="isDrawerOpen = !isDrawerOpen"></v-app-bar-nav-icon>
       <v-app-bar-title>
-        <v-btn><strong>Fácil</strong></v-btn>
+        <v-btn to="#"><strong>Fácil</strong></v-btn>
       </v-app-bar-title>
 
       <template #append>
@@ -210,7 +214,68 @@
     <!--MAIN-->
     <v-main>
       <v-container fluid>
-        <h2>Início</h2>
+        <h2 class="mb-6">Início</h2>
+
+        <v-card flat class="border mb-4">
+          <div class="d-flex justify-space-between">
+            <v-card-title>Últimos Usuários Cadastrados</v-card-title>
+
+            <!-- Button New User -->
+            <v-card-title>
+              <v-btn @click="isDialogOpen = true" variant="tonal">Criar novo Usuário</v-btn>
+              <v-dialog v-model="isDialogOpen" width="85vh">
+                <v-card>
+                  <v-card-text>
+                    Hey
+                    <v-btn color="white">Enviar</v-btn>
+                    <v-btn color="error">fechar</v-btn>
+                  </v-card-text>
+
+                </v-card>
+              </v-dialog>
+            </v-card-title>
+
+
+          </div>
+          <v-table>
+            <thead>
+              <tr>
+                <th>Nome</th>
+                <th>Email</th>
+                <th>Função</th>
+                <th>Ações</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>John Doe</td>
+                <td>john.doe@email.com</td>
+                <td>User</td>
+                <td>
+                  <v-btn icon="mdi-pencil" variant="tonal" color="primary"></v-btn>
+                </td>
+              </tr>
+              <tr>
+                <td>Jane Doe</td>
+                <td>jane.doe@email.com</td>
+                <td>Manager</td>
+                <td>
+                  <v-btn icon="mdi-pencil" variant="tonal" color="primary"></v-btn>
+                </td>
+              </tr>
+              <tr>
+                <td>No More</td>
+                <td>nmore@email.com</td>
+                <td>Admin</td>
+                <td>
+                  <v-btn icon="mdi-pencil" variant="tonal" color="primary"></v-btn>
+                </td>
+              </tr>
+            </tbody>
+          </v-table>
+        </v-card>
+
+
         <v-row>
           <v-col v-for="item in items" cols="12" sm="6" md="4" lg="3">
             <!--construção do card-->
@@ -244,8 +309,13 @@
 <script setup>
 import HelloWorld from '@/components/HelloWorld.vue'
 import { ref } from 'vue';
+
 //Sidebar Config
 const isDrawerOpen = ref(false);
+//Sidebar Sub Menu
+const isSubSide = ref(false);
+//Show Dialog
+const isDialogOpen = ref(false);
 //Theme Config Toogle
 const theme = ref('dark')
 function onToggleTheme() {
---
+++ src/Login.vue
@@ -0,0 +1,95 @@
+<template>
+  <v-container>
+    <v-row allign="center" justify="center">
+      <v-col cols="12" sm="10">
+        <v-card class="elevation-6 mt-10">
+          <v-window v-model="step">
+            <v-window-item :value="1">
+              <v-row>
+                <v-col cols="12" sm="6">
+                  <v-cart-text class="mt-12">
+                    <h4 class="text-center">Login in to Your Account</h4>
+                    <h6 class="text-center grey--text">Login in to your account so you can continue building <br>and editing your onbordarding flows</h6>
+                    <v-row allign="center" justify="center">
+                      <v-col cols="12" sm="8">
+
+                        <v-text-field 
+                        label="Email" 
+                        outlined 
+                        dense 
+                        color="blue" 
+                        autocomplete="false" 
+                        class="mt-16">
+                        </v-text-field>
+                        <v-text-field 
+                        label="Password" 
+                        outlined 
+                        dense 
+                        color="blue" 
+                        autocomplete="false" 
+                        type="password">
+                        </v-text-field>
+
+                        <v-row>
+                          <v-col cols="12" sm="7">
+                            <v-checkbox 
+                            label="Remember Me" 
+                            class="mt-n1" 
+                            color="blue">
+                          </v-checkbox>
+                          </v-col>
+                          <v-col cols="12" sm="5">
+                            <span class="caption blue--text" to="#">Forgot Password</span>
+                          </v-col>
+                        </v-row>
+                        <v-btn color="blue" dark block title>Log in</v-btn>
+
+                        <h5 
+                        class=" text-center grey--text mt-4 mb-3"
+                        >Or Log in using </h5>
+                        <div class="d-flex justify-space-between align-center mx-10 mb-16">
+                        <v-btn depressed outlined color="grey">
+                          <v-icon color="red">fab fa-google</v-icon>
+                        </v-btn>
+                        <v-btn depressed outlined color="grey">
+                          <v-icon color="blue">fab fa-facebook</v-icon>
+                        </v-btn>
+                        <v-btn depressed outlined color="grey">
+                          <v-icon color="light-blue lighten">fab fa-google</v-icon>
+                        </v-btn>
+                      </div>
+                      </v-col>
+                    </v-row>
+                  </v-cart-text>
+                </v-col>
+                <v-col cols="12" sm="6" class="blue rounded-bl-xl"></v-col>
+              </v-row>
+            </v-window-item>
+          </v-window>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+
+export default {
+  data: () => ({
+    step: 1
+  }),
+  propos: {
+    source: String
+  }
+}
+</script>
+
+<style scoped>
+.v-aplication .rounded-bl-xl {
+  border-bottom-left-radius: 300px !important;
+}
+
+.v-aplication .rounded-br-xl {
+  border-bottom-right-radius: 300px !important;
+}
+</style>
